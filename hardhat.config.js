f060f93dc0225d1b4869b37ef04e7ddd26dca87e
hernan venezia <hernan.venezia@gmail.com>
2025-07-11 09:10:19 -0300

    fixed packages

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,5 +2,14 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 module.exports = {
-  solidity: "0.8.28",
+  solidity: {
+    version: "0.8.28",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+      viaIR: true
+    }
+  },
 };
